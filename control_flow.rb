90caed6a7eef6f1a2b4b51c6a5a4624f1c091df6
Doreen <doreenlulu97@gmail.com>
2022-10-20 02:13:00 +0300

    commit tree 739b3c7c0357fe687b5b16115e384e9890438ca0

--- control_flow.rb
+++ control_flow.rb
@@ -1,16 +1,44 @@
+# require 'pry'
+
 def admin_login(username, password)
-  # your code here
+ 
+  if (username == "ADMIN" || username == "admin") && password == "12345"
+    puts "Access granted"
+  else
+    puts "Access denied"
 end
+
 
 def hows_the_weather(temperature)
-  # your code here
-end
+  let response
+  
+response = if temperature < 40
+              puts "It's brisk out there!"
+              elsif temperature >40 && temperature <65
+                puts "It's a little chilly out there!"
+            elsif temperature > 85
+              puts "It's too dang hot out there!"
+          end
+
 
 def fizzbuzz(num)
-  # your code here
+ 
+  
 end
 
 def calculator(operation, num1, num2)
-  # your code here
+  
+  case operation
+when "+"
+  num1 + num2
+when "-"
+  num1 - num2
+when "*"
+  num1 * num2
+when "/"
+  num1 / num2
+else
+  "Invalid operation!"
 end
 
+# binding.pry
